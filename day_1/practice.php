e20a73fa56e8061e58397a3099134a0870a97bca
Clifford Nelson <droffilcnoslen@gmail.com>
2013-05-08 11:55:20 -0700

    adding more logic and variables

--- day_1/practice.php
+++ day_1/practice.php
@@ -1,9 +1,25 @@
-<!DOCTYPE html>
+	<?php //create status vars - possible values are 'success' or 'error'
+	$status = 'success';
+	$message = 'Hello World';
+	//Logic to control the message text based on the status
+	if($status == 'success'){$message = 'You\'re Successfull';}
+	else{$message = 'Sorry, but you suck!';} 
+	?>
+<!DOCTYPE>
 <html>
 <head>
+	<style>
+	.error {background-color: #F00;}
+	.success {background-color: #0F0;}
+	</style>
 </head>
-
 <body>
-Hello World
+	<div class="<?php echo $status; ?>">
+		<h2>
+		<?php 
+		//comment secret
+		echo $message; ?>
+		</h2>
+	</div>
 </body>
 </html>
