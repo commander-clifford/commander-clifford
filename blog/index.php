b7a951d1a380e5c0e4b84e3fe2ceeedb21f92a00
Clifford Nelson <droffilcnoslen@gmail.com>
2013-05-17 12:57:45 -0700

    php  databases

--- blog/index.php
+++ blog/index.php
@@ -3,71 +3,48 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>Comm-Cliff PHP BLOBG</title>
+<title>Comm-Cliff PHP BLOG</title>
 <link rel="stylesheet" type="text/css" href="style.css">
+<link href='http://fonts.googleapis.com/css?family=Special+Elite|Amatic+SC|Marvel:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
 </head>
-
 <body>
-	<div class="container">
-		<header>
-			<h1>Commander-Cliff's BloBg</h1>
+	
+		<header class="cf">
+			<div class="heading">
+				<h1>Commander-Clifford's Blog</h1>
+			</div>
+			<nav>
+				<ul>
+					<li><a href="index.php">Home</a></li>
+					<li><a href="index.php?page=blog">Blog</a></li>
+					<li><a href="index.php?page=links">Links</a></li>
+				</ul>
+			</nav>
 		</header>
-		<div class="wrapper">
-
-		
-
 		<main>
-
 			<?php 
-			//set up a query to gett the latest two posts that are public
-			$query = 'SELECT title, body, date, category_id, post_id 
-						FROM posts
-						WHERE is_public = 1
-						ORDER BY date DESC
-						LIMIT 2';
-			//run it and check to make sure the result contains posts
-			if($result = $db->query($query)	):		
-			 ?>
-
-			<h2>Most Recent Blobs</h2>
-
-				<?php 
-					//loop thru the list of results
-				while ($row = $result->fetch_assoc() ):
-				 ?>
-
-			<article class="posts">
-				<h3><?php echo $row['title']; ?></h3>
-				<div class="postmeta">Posted on <?php echo $row['date']; ?> | in the category NAME</div>
-				<p><?php echo $row['body']; ?></p>
-			</article>
-
-				<?php 
-				endwhile;
-				 ?>
-
-
-
-			<?php else: ?>
-				<h2>No posts to show</h2>
-			<?php endif; ?>
+				//logic to load the correct page contents.
+				//URI will look like domain/index.php?page=something
+				switch( $_GET['page'] ){
+					case 'links':
+						include('content_links.php');
+					case 'blog':
+						include('content_blog.php');
+					break;
+					default:
+						include('content_home.php');
+				}
+			?>
 		</main>
-
-
-
 		<aside>
 			<?php include('sidebar.php') ?>
 		</aside>
-
-	
-
-		</div><!-- end class="wrapper" -->
 		
 		<footer>
 			<p>footer fetish?</p>
 		</footer>
 
-	</div><!--end div class="container" -->
+	
 
 </body>
 </html>	
