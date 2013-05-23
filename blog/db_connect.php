c67282576552a4d1c3b6da9bce97ef55236847e4
commander-clifford <droffilcnoslen@gmail.com>
2013-05-23 12:13:52 -0700

    oops added

--- blog/db_connect.php
+++ blog/db_connect.php
@@ -4,5 +4,5 @@ $db = new mysqli('localhost','commander-cliff','173.164.the-PEOPLE','blog_crn032
 
 //if error connecting, kill the page
 if($db->connect_errno > 0 ){
-	die('Unable to connect to the database');
+	die('Oops, Unable to connect to the database');
 }
